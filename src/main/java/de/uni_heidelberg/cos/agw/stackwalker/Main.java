8f8a87bcea8553a5645f43cda6c4e9a253d8cfa5
Burkhard Hoeckendorf <b.hoeckendorf@web.de>
2015-03-28 02:05:18 +0000

    Rename main class to Main

---
+++ src/main/java/de/uni_heidelberg/cos/agw/stackwalker/Main.java
@@ -0,0 +1,35 @@
+package de.uni_heidelberg.cos.agw.stackwalker;
+
+import de.uni_heidelberg.cos.agw.stackwalker.ui.MainFrame;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (UnsupportedLookAndFeelException e) {
+        } catch (ClassNotFoundException e) {
+        } catch (InstantiationException e) {
+        } catch (IllegalAccessException e) {
+        }
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    //DataSetModel dataSetModel = new DataSetModel();
+                    DataSetTreeModel dataSetTreeModel = new DataSetTreeModel(new DataSetTreeModelNode());
+                    MainFrame frame = new MainFrame(dataSetTreeModel);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.pack();
+                    frame.setLocationRelativeTo(null);
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
